6ddd31ebed5dcdd1ce0de7d6a9d2e8db9e0fb041
GustavoSilverio <gustavosk290905@gmail.com>
2023-03-14 11:09:24 -0300

    Criação Footer Parcial

--- src/Components/Template/Footer/Footer.jsx
+++ src/Components/Template/Footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './footer.scss';
+import Icon from '@mdi/react';
+import { mdiWhatsapp, mdiLinkedin, mdiGithub, mdiInstagram } from '@mdi/js';
 
 const Footer = () => {
     return (
         <div className="footer">
-            <h1>Hello, footer!</h1>
+            <p>Copyright © <span>Gustavo Oliveira</span> All Right Reserved</p>
+            <div className="midias">
+                <Icon path={mdiWhatsapp} size={1} color="#FF005C"/>
+                <Icon path={mdiLinkedin} size={1} color="#FF005C"/>
+                <Icon path={mdiGithub} size={1} color="#FF005C"/>
+                <Icon path={mdiInstagram} size={1} color="#FF005C"/>
+            </div>
         </div>
     );
 }
